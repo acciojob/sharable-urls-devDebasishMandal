ca68a27b0e49f236fa5c9b601e962d63ea7b6d85
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2023-12-19 15:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+let head=document.getElementById("url");
+head.textContent="https://localhost:8080/";
+let btn=document.getElementById("btn");
+let nam,year;
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+     nam=document.getElementById("name").value;
+     year=document.getElementById("year").value;
+});
+
+if(nam && year){
+    head.textContent=`https://localhost:8080/?name=${nam}&year=${year}`;
+}else if(nam){
+    head.textContent=`https://localhost:8080/?name=${nam}`;
+} else if(year){
+    head.textContent=`https://localhost:8080/?year=${year}`;
+}
